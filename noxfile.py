0bc0e10faf448f1294e3e14070199f47e9e10f80
Paul Gessinger <hello@paulgessinger.com>
2025-03-05 10:28:03 +0100

    remove 3.10 from nox file

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ nox.options.default_venv_backend = "uv"
 
 
 @nox.session
-@nox.parametrize("python", ["3.10", "3.11", "3.12", "3.13"])
+@nox.parametrize("python", ["3.11", "3.12", "3.13"])
 def tests(session: nox.Session):
     session.run(
         "uv",
