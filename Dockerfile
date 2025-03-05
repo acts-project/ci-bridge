5281613037e22265b4f93717b32c6cfd31be2651
Paul Gessinger <hello@paulgessinger.com>
2025-03-05 10:21:34 +0100

    drop 3.10, nox, update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10-slim
+FROM python:3.13-slim
 
 ENV USER=ci_relay
 RUN adduser --gecos "" --disabled-password $USER
---
+++ noxfile.py
@@ -0,0 +1,17 @@
+import nox
+
+nox.options.default_venv_backend = "uv"
+
+
+@nox.session
+@nox.parametrize("python", ["3.10", "3.11", "3.12", "3.13"])
+def tests(session: nox.Session):
+    session.run(
+        "uv",
+        "run",
+        "--frozen",
+        "--python",
+        session.bin + "/python",
+        "--active",
+        "pytest",
+    )
